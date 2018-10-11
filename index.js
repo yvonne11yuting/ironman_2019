50c1fabf60e73d0d0c1b3ed7e7dd3e02ab1792ca
YuTing Huang <yutingeko@gmail.com>
2018-10-11 16:12:51 +0800

    button components

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Button from './src/components/button';
+
 const App = () => {
   return (
     <div>
       Hello World!
+      <Button text="按鈕按鈕"/>
+      <Button text="按鈕二"/>
+      <Button text="按鈕三"/>
+      <Button text="按鈕四"/>
+      <Button text="按鈕五"/>
     </div>
   );
 };

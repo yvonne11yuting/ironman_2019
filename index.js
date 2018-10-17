4f2848d24cc0150924d2ff2adb94c136e312f0ec
yvonne11yuting <yvonne11yuting@gmail.com>
2018-10-17 11:23:46 +0800

    new_todo

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
-};
+import NewTodo from './src/components/new_todo';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: []
+    }
+    this.addNewTodo = this.addNewTodo.bind(this);
+  }
+
+  addNewTodo(text) {
+    let newItem = {
+      text,
+      completed: false
+    }
+    this.setState({
+      todoList: [...this.state.todoList, newItem]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <NewTodo addNewTodo={this.addNewTodo}/>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App/>, document.getElementById('app'));
---
+++ src/components/new_todo.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class NewTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoVal: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      todoVal: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addNewTodo(this.state.todoVal);
+    this.setState({ todoVal: "" });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.todoVal}
+          onChange={this.handleChange}
+        />
+      </form>
+    );
+  }
+}
+
+export default NewTodo;

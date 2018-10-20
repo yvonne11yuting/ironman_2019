1a62f046d02c7d38617e6e9100935388b4048490
yvonne11yuting <yvonne11yuting@gmail.com>
2018-10-21 02:29:50 +0800

    basic structure

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./src/reducers/index";
+
 import App from "./src/components/app";
 import "./src/styles/index.scss";
 
-ReactDOM.render(<App/>,  document.getElementById("app"));
+ReactDOM.render(
+  <Provider store={createStore(rootReducer)}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);

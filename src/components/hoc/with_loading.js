7d6408b62ad1d137dfdf62f5e68231332b08d866
yvonne11yuting <yvonne11yuting@gmail.com>
2018-10-31 08:52:03 +0800

    prettify

--- src/components/hoc/with_loading.js
+++ src/components/hoc/with_loading.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const withLoading = (WrappedComponent) => ({isLoading, ...props}) => {
   return isLoading ?
-  <div className="loading-block"><FontAwesomeIcon icon={faSpinner} size="2x" color="#777" pulse /></div> :
+  <div className="loading-block">
+    <FontAwesomeIcon icon={faSpinner} size="2x" color="#777" pulse />
+  </div> :
   <WrappedComponent {...props} />;
 };
 

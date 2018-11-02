1ffb53d458e89d05498e7a1300fe3ee24547d318
yvonne11yuting <yvonne11yuting@gmail.com>
2018-11-02 12:02:04 +0800

    add lang selection

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,43 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 import { FormattedMessage } from "react-intl";
 
 const navList = [
   { to: "/", textId: "home" },
   { to: "/todos", textId: "todos" },
-  { to: "/news", textId: "news" },
-]
+  { to: "/news", textId: "news" }
+];
 
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header-heading"><FormattedMessage id="heading"  description="heading"/></h1>
+      <h1 className="header-heading">
+        <FormattedMessage id="heading" description="heading" />
+      </h1>
       <nav>
         <ul className="header-nav-list">
-          { navList.map(({to, textId}, index) => (
+          {navList.map(({ to, textId }, index) => (
             <li key={index} className="header-nav-list-item">
               <NavLink
                 exact
                 className="list-item-anchor"
                 activeClassName="list-item-anchor-active"
-                to={to}>
-                <FormattedMessage id={textId} description="navigation"/>
+                to={to}
+              >
+                <FormattedMessage id={textId} description="navigation" />
               </NavLink>
             </li>
           ))}
         </ul>
-        </nav>
+      </nav>
+      <div className="lang">
+        <span>Language</span>
+        <ul className="lang-options">
+          <li className="icon icon-taiwan"></li>
+          <li className="icon icon-japan"></li>
+          <li className="icon icon-us"></li>
+        </ul>
+      </div>
     </header>
   );
 };
